1a54d6fc6634363219829468b2f7ccc76cb4e2da
dai18088 <dai18088@uom.edu.gr>
2023-02-19 18:48:16 +0200

    initial commit

---
+++ androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.noteappkmm.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.noteappkmm.android.note_detail.NoteDetailScreen
+import com.example.noteappkmm.android.note_list.NoteListScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "note_list") {
+
+                    composable(route = "note_list") {
+                        NoteListScreen(navController = navController)
+                    }
+
+                    composable(
+                        route = "note_detail/{noteId}",
+                        arguments = listOf(
+                            navArgument(name = "noteId") {
+                                type = NavType.LongType
+                                defaultValue = -1L
+                            }
+                        )
+                    ) { backStackEntry ->
+                        val noteId = backStackEntry.arguments?.getLong("noteId") ?: -1L
+                        NoteDetailScreen(noteId = noteId, navController = navController)
+                    }
+                }
+            }
+        }
+    }
+}
